2b89ce7bb8f1443430a7bb58deeb56e93a0db8f0
Logan Zartman <logan.zartman@utexas.edu>
2019-05-10 15:54:36 -0500

    normal coloring

--- shaders/cloth.fs
+++ shaders/cloth.fs
@@ -14,6 +14,7 @@ void main() {
     vec3 lighting = vec3(max(0, dot(light_direction.xyz, real_normal)));
     vec3 ambient = vec3(0.3,0.32,0.4);
     frag_color = vec4((ambient + lighting) * color, 1);
+    frag_color = vec4(real_normal * 0.5 + 0.5, 1);
 
   //  frag_color = vec4(gs_debug_color, 1);
     if (barycentric.x < 0.1 && barycentric.y > 0.9)
