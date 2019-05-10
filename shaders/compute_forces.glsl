79d75cf114dac41306c7086cbc55e6705a7a196a
Logan Zartman <logan.zartman@utexas.edu>
2019-05-10 01:53:50 -0500

    refactor

--- shaders/compute_forces.glsl
+++ shaders/compute_forces.glsl
@@ -31,30 +31,35 @@ bool in_bounds(ivec2 pos) {
     return pos.x >= 0 && pos.y >= 0 && pos.x < cloth_dimension.x && pos.y < cloth_dimension.y;
 }
 
+vec3 compute_force(ivec2 pos, ivec2 neighbor, float constraint_len) {
+    if (!in_bounds(neighbor)) {
+        return vec3(0);
+    }
+    uint c = index(pos);
+    uint n = index(neighbor);
+    
+    float dist = distance(vertex[c].position, vertex[n].position);
+    vec3 offset = normalize(vertex[n].position - vertex[c].position);
+    return offset * (dist - constraint_len) * stiffness;
+}
+
 void main() {
     ivec2 pos = ivec2(gl_WorkGroupID.xy);
     uint current = index(pos);
 
     vec3 force = vec3(0);
-    // if (pos.x > 0 && pos.y > 0 && pos.x < cloth_dimension.x-1 && pos.y < cloth_dimension.y-1)
     for (int dx = -1; dx <= 1; ++dx) {
         for (int dy = -1; dy <= 1; ++dy) {
             if (dx == 0 && dy == 0) {
                 continue;
             }
             ivec2 neighbor_pos = pos + ivec2(dx, dy);
-            if (!in_bounds(neighbor_pos)) {
-                continue;
-            }
-            uint neighbor = index(neighbor_pos);
 
             float constraint_len = 1.0;
             if (dx != 0 && dy != 0)
                 constraint_len *= sqrt(2.0);
-
-            float dist = distance(vertex[current].position, vertex[neighbor].position);
-            vec3 offset = normalize(vertex[neighbor].position - vertex[current].position);
-            force += offset * (dist - constraint_len) * stiffness;
+            
+            force += compute_force(pos, neighbor_pos, constraint_len);
         }
     }
 
