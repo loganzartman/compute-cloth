64ae1be671af4bea787a320819e6325c5ba3b70e
Logan Zartman <logan.zartman@utexas.edu>
2019-05-10 15:43:01 -0500

    two-sided lighting

--- shaders/cloth.fs
+++ shaders/cloth.fs
@@ -8,8 +8,10 @@ in vec3 normal;
 out vec4 frag_color;
 
 void main() {
+    vec3 real_normal = gl_FrontFacing ? normal : -normal;
+
     vec3 color = vec3(0.88,0.88,0.88);
-    vec3 lighting = vec3(max(0, dot(light_direction.xyz, normal)));
+    vec3 lighting = vec3(max(0, dot(light_direction.xyz, real_normal)));
     vec3 ambient = vec3(0.3,0.32,0.4);
     frag_color = vec4((ambient + lighting) * color, 1);
 
